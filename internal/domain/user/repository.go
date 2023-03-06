0ad6e0369afd4bdacee8b8023abee04e02d8e7a9
alexandre <alexandre@indev.net.br>
2023-03-05 21:19:28 -0300

    feat: add repository example

---
+++ internal/domain/user/repository.go
@@ -0,0 +1,36 @@
+package user
+
+import (
+	"context"
+	"devalexandre/golang-ddd-template/internal/infra/pgx"
+)
+
+func NewRepository(db pgx.IDatabase) IRepository {
+	return &Repository{
+		DB: db,
+	}
+}
+
+func (r *Repository) Insert(ctx context.Context, user *User) error {
+	return r.DB.Insert(ctx, "users", user)
+}
+
+func (r *Repository) Update(ctx context.Context, user *User) error {
+	return r.DB.Update(ctx, "users", user)
+}
+
+func (r *Repository) Delete(ctx context.Context, user *User) error {
+	return r.DB.Delete(ctx, "users", user)
+}
+
+func (r *Repository) Find(ctx context.Context, id string) (*User, error) {
+	user := &User{}
+	err := r.DB.Select(ctx, user, "SELECT * FROM users WHERE id = $1 LIMIT 1", id)
+	return user, err
+}
+
+func (r *Repository) FindAll(ctx context.Context) ([]*User, error) {
+	var users []*User
+	err := r.DB.Query(ctx, &users, "SELECT * FROM users")
+	return users, err
+}
